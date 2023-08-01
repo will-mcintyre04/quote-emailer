16ce41b7d86a0058f14ee01d96207e99e8a999de
Will McIntyre <william.d.j.mcintyre@gmail.com>
2023-08-01 13:38:41 -0400

    command line argument implimentation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-import schedule
+import argparse
 import time
 
 from quote import Quote
@@ -14,5 +13,21 @@ class QuoteBot:
         print(quote, author)
 
 if __name__ == "__main__":
+    ## Create Bot
     bot = QuoteBot()
-    bot.daily_quote()
+
+    ## Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Quote of the Day Bot")
+    parser.add_argument("--list", action="store_true", help="List all subscribers")
+    parser.add_argument("--email", type=str, help="Email address to add to database")
+
+    args = parser.parse_args()
+
+    if args.email:
+        bot.add_subscriber(args.email)
+    elif args.list:
+        bot.list_subscribers()
+    
+    ## If no command-line arguments, send a daily quote
+    else:
+        bot.daily_quote()
