77e53b04c4615339390817d5435fd756297bf984
Will McIntyre <william.d.j.mcintyre@gmail.com>
2023-08-24 10:58:46 -0400

    updated CLI description/title

--- motivation_mailer.py
+++ motivation_mailer.py
@@ -10,18 +10,20 @@ def main():
 
     try:   
         # Parse command-line arguments
-        parser = argparse.ArgumentParser(description="Sends motivational quotes fetched from ZenQuotes API\
-                                        (https://zenquotes.io/) to email addresses stored in a local database.")
+        parser = argparse.ArgumentParser(description="Motivation Mailer: Send quotes fetched from ZenQuotes API\
+                                            (https://zenquotes.io/) to email addresses stored in a database.",
+                                        epilog='For further documentation and source code, view: \
+                                            https://github.com/will-mcintyre04/quote-emailer')
         parser.add_argument("--status", "-s", action="store_true",
-                            help="Displays configuration environment and listsS all subscribers")
+                            help="Displays configuration environment and lists all subscribers")
         parser.add_argument("--add", "-a", type=str, nargs="+",
                             help="Email address(es) to add to database")
         parser.add_argument("--delete", "-d", type=str, nargs="+",
                             help="Email address(es) to delete from database")
         parser.add_argument("--database", "-db", type=str,
-                            help="Database configuration ('production' or 'development')")
-        parser.add_argument("--email", "-e", type=str, help="Sending email address")
-        parser.add_argument("--password", "-p", type=str, help="Sending email password")
+                            help="Sets database configuration ('production' or 'development')")
+        parser.add_argument("--email", "-e", type=str, help="Sets the sending email address")
+        parser.add_argument("--password", "-p", type=str, help="Sets the sending email password")
 
         args = parser.parse_args()
 
